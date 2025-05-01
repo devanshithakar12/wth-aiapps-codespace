92d3449fe6a02319461aa37acd94540c65df4530
Pete Rodriguez <pedrorod@microsoft.com>
2025-05-01 17:19:13 +0000

    Updated deploy.sh to be silent

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -2,11 +2,11 @@
 
 # Include functions
 source ./functions.sh
-
 # Default values
 LOCATION="East US 2"
 DOCUMENT_INTELLIGENCE_LOCATION="East US"
 OPENAI_LOCATION="East US 2"
+RESOURCE_GROUP_NAME="openai-apps-wth"
 
 # Parse arguments
 while [[ "$#" -gt 0 ]]; do
@@ -21,15 +21,11 @@ while [[ "$#" -gt 0 ]]; do
         --openai-location) OPENAI_LOCATION="$2"; shift ;;
         --document-intelligence-location) DOCUMENT_INTELLIGENCE_LOCATION="$2"; shift ;;
         --skip-local-settings-file) SKIP_LOCAL_SETTINGS_FILE=true; shift ;;
+        --silent-install) SILENT_INSTALL=true; shift ;;
         *) error_exit "Unknown parameter passed: $1" ;;
     esac
     shift
 done
-
-# Validate mandatory parameters
-if [[ -z "$SUBSCRIPTION_ID" || -z "$RESOURCE_GROUP_NAME" ]]; then
-    error_exit "Subscription ID and Resource Group Name are mandatory."
-fi
 
 # Check if Bicep CLI is installed
 if ! command -v bicep &> /dev/null; then
@@ -39,28 +35,33 @@ fi
 echo -e "\n\t\t\e[32mWHAT THE HACK - AZURE OPENAI APPS\e[0m"
 echo -e "\tcreated with love by the Americas GPS Tech Team!\n"
 
-authenticate_to_azure
+if [[ "$SILENT_INSTALL" == false ]]; then
+    # Validate mandatory parameters, if required
+    if [[ -z "$SUBSCRIPTION_ID" || -z "$RESOURCE_GROUP_NAME" ]]; then
+        error_exit "Subscription ID and Resource Group Name are mandatory."
+    fi
+    authenticate_to_azure
 
-# Set the subscription
-az account set --subscription "$SUBSCRIPTION_ID" || error_exit "Failed to set subscription."
+    # Set the subscription
+    az account set --subscription "$SUBSCRIPTION_ID" || error_exit "Failed to set subscription."
 
-# Display deployment parameters
-echo -e "The resources will be provisioned using the following parameters:"
-echo -e "\t          TenantId: \e[33m$TENANT_ID\e[0m"
-echo -e "\t    SubscriptionId: \e[33m$SUBSCRIPTION_ID\e[0m"
-echo -e "\t    Resource Group: \e[33m$RESOURCE_GROUP_NAME\e[0m"
-echo -e "\t            Region: \e[33m$LOCATION\e[0m"
-echo -e "\t   OpenAI Location: \e[33m$OPENAI_LOCATION\e[0m"
-echo -e "\t Azure DI Location: \e[33m$DOCUMENT_INTELLIGENCE_LOCATION\e[0m"
-echo -e "\e[31mIf any parameter is incorrect, abort this script, correct, and try again.\e[0m"
-echo -e "It will take around \e[32m15 minutes\e[0m to deploy all resources. You can monitor the progress from the deployments page in the resource group in Azure Portal.\n"
+    # Display deployment parameters
+    echo -e "The resources will be provisioned using the following parameters:"
+    echo -e "\t          TenantId: \e[33m$TENANT_ID\e[0m"
+    echo -e "\t    SubscriptionId: \e[33m$SUBSCRIPTION_ID\e[0m"
+    echo -e "\t    Resource Group: \e[33m$RESOURCE_GROUP_NAME\e[0m"
+    echo -e "\t            Region: \e[33m$LOCATION\e[0m"
+    echo -e "\t   OpenAI Location: \e[33m$OPENAI_LOCATION\e[0m"
+    echo -e "\t Azure DI Location: \e[33m$DOCUMENT_INTELLIGENCE_LOCATION\e[0m"
+    echo -e "\e[31mIf any parameter is incorrect, abort this script, correct, and try again.\e[0m"
+    echo -e "It will take around \e[32m15 minutes\e[0m to deploy all resources. You can monitor the progress from the deployments page in the resource group in Azure Portal.\n"
 
-read -p "Press Y to proceed to deploy the resources using these parameters: " proceed
-if [[ "$proceed" != "Y" && "$proceed" != "y" ]]; then
-    echo -e "\e[31mAborting deployment script.\e[0m"
-    exit 1
+    read -p "Press Y to proceed to deploy the resources using these parameters: " proceed
+    if [[ "$proceed" != "Y" && "$proceed" != "y" ]]; then
+        echo -e "\e[31mAborting deployment script.\e[0m"
+        exit 1
+    fi
 fi
-
 start=$(date +%s)
 
 # Create resource group
